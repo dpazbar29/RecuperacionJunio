1e6e7308531e5cae8cef07748614152395e91255
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-05-31 19:23:53 +0200

    INICIO PARSEADOR

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
-import sql.utils.DataSourceFactory
-
 fun main(args: Array<String>) {
+    val parse = ParseadorArgumentos()
+    parse.parsearArgumentos(args)
+    /*
     val appGestorCTS = AppGestorCTFS()
     val conexion = Conexion()
     val dataSource = DataSourceFactory.getDS(DataSourceFactory.DataSourceType.HIKARI)
 
     conexion.conectar(dataSource)
     appGestorCTS.menu(dataSource, args)
+     */
 }
--- src/main/kotlin/ParseadorArgumentos.kt
+++ src/main/kotlin/ParseadorArgumentos.kt
@@ -1,4 +1,50 @@
-class ParseadorArgumentos(args: Array<String>) {
-    fun parsearArgumentos() {
+@Suppress("ktlint:standard:max-line-length")
+class ParseadorArgumentos {
+    fun parsearArgumentos(args: Array<String>): Array<String> {
+        var resultado: Array<String> = arrayOf()
+        when (args[0]) {
+            "g" -> {
+                if (args.size != 3) {
+                    throw IllegalArgumentException(
+                        "No hay suficientes argumentos para el comando 'g'. Se esperaban 2 argumentos (grupoID, descripción).",
+                    )
+                }
+                resultado = arrayOf(args[1], args[2])
+            }
+            "p" -> {
+                if (args.size != 4) {
+                    throw IllegalArgumentException(
+                        "No hay suficientes argumentos para el comando 'p'. Se esperaban 3 argumentos (ctfID, grupoID, puntuación).",
+                    )
+                }
+                resultado = arrayOf(args[1], args[2], args[3])
+            }
+            "t" -> {
+                if (args.size != 2) {
+                    throw IllegalArgumentException("No hay suficientes argumentos para el comando 't'. Se esperaba 1 argumento (grupoID).")
+                }
+                resultado = arrayOf(args[1])
+            }
+            "e" -> {
+                if (args.size != 3) {
+                    throw IllegalArgumentException(
+                        "No hay suficientes argumentos para el comando 'e'. Se esperaban 2 argumentos (ctfID, grupoID).",
+                    )
+                }
+                resultado = arrayOf(args[1], args[2])
+            }
+            "p" -> {
+                if (args.size != 4) {
+                    throw IllegalArgumentException(
+                        "No hay suficientes argumentos para el comando 'p'. Se esperaban 3 argumentos (ctfID, grupoID, puntuación).",
+                    )
+                }
+                resultado = arrayOf(args[1], args[2], args[3])
+            }
+            else -> {
+                throw IllegalArgumentException("Comando desconocido: ${args[0]}")
+            }
+        }
+        return resultado
     }
 }
