99daef0393cac72a749d07bb78cd9dde60677e28
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-06-03 16:34:31 +0200

    Estrucutra básica main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,10 @@ import sql.utils.DataSourceFactory
 
 fun main(args: Array<String>) {
     val dataSource = DataSourceFactory.getDS(DataSourceFactory.DataSourceType.HIKARI)
+
+    val conexion = Conexion()
+    conexion.conectar(dataSource)
+
     val argumentos = ParseadorArgumentos()
     val comandos: Array<String> = argumentos.parsearArgumentos(args)
 
