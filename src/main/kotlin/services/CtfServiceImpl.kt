b787cc9517a60f1254162d3cb132894f9f07d405
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-06-04 09:00:30 +0200

    Arreglo CtfDAO

--- src/main/kotlin/services/CtfServiceImpl.kt
+++ src/main/kotlin/services/CtfServiceImpl.kt
@@ -2,13 +2,14 @@ package services
 
 import dao.CtfDAO
 import dao.entity.CtfEntity
+import javax.sql.DataSource
 
 /**
  * Clase que emplea los métodos del Servicio del CTF
  *
  * @property ctfDAO: objeto del DAO del CTF donde se lleva a cabo el código de estas funciones
  */
-class CtfServiceImpl(private val ctfDAO: CtfDAO) : CtfService {
+class CtfServiceImpl(private val ctfDAO: CtfDAO, private val dataSource: DataSource) : CtfService {
     /**
      * Método que crea un nuevo CTF con sus datos correspondientes
      *
@@ -54,21 +55,59 @@ class CtfServiceImpl(private val ctfDAO: CtfDAO) : CtfService {
     /**
      * Método que obtiene los datos de un CTF por un ID dado
      *
-     * @param id: ID del CTF que queremos obtener
+     * @param id: Id del CTF que queremos obtener
      * @return CtfEntity?: Devuelve el CTF cuyo ID se ha dado
      */
     override fun obtenerPorIDCtf(id: Int): CtfEntity? {
-        return ctfDAO.obtenerPorIDCtf(id)
+        val sqlID = "SELECT * FROM CTFS WHERE ctfID = ?"
+
+        return dataSource.connection.use { connID ->
+            connID.prepareStatement(sqlID).use { stmtID ->
+                stmtID.setInt(1, id)
+
+                val rs = stmtID.executeQuery()
+
+                if (rs.next()) {
+                    CtfEntity(
+                        ctfID = rs.getInt("ctfid"),
+                        grupoID = rs.getInt("grupoid"),
+                        puntuacion = rs.getInt("puntuacion"),
+                    )
+                } else {
+                    null
+                }
+            }
+        }
     }
 
     /**
      * Método que obtiene los datos de todos los CTFs en los que ha participado un grupo
      *
-     * @param id: ID del grupo que queremos obtener
+     * @param id: Id del grupo que queremos obtener
      * @return List<CtfEntity>: Devuelve una lista con los CTFs en los que ha participado el grupo dado
      */
     override fun obtenerPorIDGrupo(id: Int): List<CtfEntity> {
-        return ctfDAO.obtenerPorIDGrupo(id)
+        val sqlID = "SELECT * FROM CTFS WHERE grupoID = ?"
+
+        return dataSource.connection.use { connID ->
+            connID.prepareStatement(sqlID).use { stmtID ->
+                stmtID.setInt(1, id)
+
+                val rs = stmtID.executeQuery()
+                val ctfs = mutableListOf<CtfEntity>()
+
+                while (rs.next()) {
+                    ctfs.add(
+                        CtfEntity(
+                            ctfID = rs.getInt("ctfid"),
+                            grupoID = rs.getInt("grupoid"),
+                            puntuacion = rs.getInt("puntuacion"),
+                        ),
+                    )
+                }
+                ctfs
+            }
+        }
     }
 
     /**
@@ -82,34 +121,84 @@ class CtfServiceImpl(private val ctfDAO: CtfDAO) : CtfService {
         idGrupo: Int,
         idCtf: Int,
     ): CtfEntity? {
-        return ctfDAO.obtenerPorIDGrupoIDCtf(idGrupo, idCtf)
+        val sqlIDs = "SELECT * FROM CTFS WHERE grupoID = ? AND ctfID = ?"
+
+        return dataSource.connection.use { connIDs ->
+            connIDs.prepareStatement(sqlIDs).use { stmtIDs ->
+                stmtIDs.setInt(1, idGrupo)
+                stmtIDs.setInt(2, idCtf)
+
+                val rs = stmtIDs.executeQuery()
+
+                if (rs.next()) {
+                    CtfEntity(
+                        ctfID = rs.getInt("ctfid"),
+                        grupoID = rs.getInt("grupoid"),
+                        puntuacion = rs.getInt("puntuacion"),
+                    )
+                } else {
+                    null
+                }
+            }
+        }
     }
 
     /**
      * Método que obtiene la puntuación de un grupo en un CTF
      *
-     * @param idGrupo: ID del grupo a obtener la puntuación
-     * @param idCtf: ID del CTF del que queremos la puntuación
+     * @param idGrupo: Id del grupo a obtener la puntuación
+     * @param idCtf: Id del CTF del que queremos la puntuación
      * @return Int: Devuelve la puntuación del grupo en el CTF
      */
     override fun obtenerPuntuacionPorIDGrupoIDCtf(
         idGrupo: Int,
         idCtf: Int,
     ): Int {
-        return ctfDAO.obtenerPuntuacionPorIDGrupoIDCtf(idGrupo, idCtf)
+        val sqlID = "SELECT puntuacion FROM CTFS WHERE ctfID = ? AND grupoID = ?"
+        var puntuacion = 0
+        dataSource.connection.use { connID ->
+            connID.prepareStatement(sqlID).use { stmtID ->
+                stmtID.setInt(1, idCtf)
+                stmtID.setInt(2, idGrupo)
+
+                val rs = stmtID.executeQuery()
+                val ctfs = mutableListOf<CtfEntity>()
+
+                if (rs.next()) {
+                    puntuacion = rs.getInt("puntuacion")
+                }
+                ctfs
+            }
+        }
+
+        return puntuacion
     }
 
     /**
      * Método que obtiene la posición de un grupo en un CTF
      *
-     * @param idGrupo: ID del grupo a obtener la posición
-     * @param idCtf: ID del CTF del que queremos la posición
+     * @param idGrupo: Id del grupo a obtener la posición
+     * @param idCtf: Id del CTF del que queremos la posición
      * @return Int: Devuelve la posición del grupo en el CTF
      */
     override fun obtenerPosicionGrupoEnCtf(
         idGrupo: Int,
         idCtf: Int,
     ): Int? {
-        return ctfDAO.obtenerPosicionGrupoEnCtf(idGrupo, idCtf)
+        val sql = "SELECT grupoid, puntuacion, RANK() OVER (PARTITION BY CTFid ORDER BY puntuacion DESC) AS posicion FROM CTFS WHERE CTFid = ?"
+
+        dataSource.connection.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                stmt.setInt(1, idCtf)
+
+                val rs = stmt.executeQuery()
+                while (rs.next()) {
+                    if (rs.getInt("grupoid") == idGrupo) {
+                        return rs.getInt("posicion")
+                    }
+                }
+            }
+        }
+        return null
     }
 }
