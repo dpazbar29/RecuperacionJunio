dcc3e69f3e7e028ecb41fff0ee8ee119b9f0fe96
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-06-06 08:37:33 +0200

    Documetacion de parseador argumentos

--- src/main/kotlin/ParseadorArgumentos.kt
+++ src/main/kotlin/ParseadorArgumentos.kt
@@ -1,5 +1,16 @@
 @Suppress("ktlint:standard:max-line-length")
+
+/**
+ * Clase que realiza el parseo de argumentos
+ */
 class ParseadorArgumentos : Salida {
+
+    /**
+     * Método que aplica las funciones pertinentes según el comando dado
+     *
+     * @param args: Argumentos que recibe el programa
+     * @return Array<String>: Array con los comandos parseados
+     */
     fun parsearArgumentos(args: Array<String>): Array<String> {
         require(args.isNotEmpty()) { "No se han proporcionado argumentos" }
 
@@ -14,7 +25,9 @@ class ParseadorArgumentos : Salida {
                         verificarEsEntero(parametros[0], "grupoID")
                         verificarEsEntero(parametros[1], "ctfID")
                     }
-                "-t", "-l", "-c" -> verificarNumeroParametros(comando, parametros, 1) { verificarEsEntero(parametros[0], "grupoID") }
+                "-t", "-l", "-c" -> verificarNumeroParametros(comando, parametros, 1) {
+                    verificarEsEntero(parametros[0], "grupoID")
+                }
                 "-e" ->
                     verificarNumeroParametros(comando, parametros, 2) {
                         verificarEsEntero(parametros[0], "grupoID")
@@ -28,6 +41,14 @@ class ParseadorArgumentos : Salida {
         return arrayOf(comando, *parametros)
     }
 
+    /**
+     * Método privado que verifica que el número de parámetros obtenidos es el correcto
+     *
+     * @param comando: Comando a verificar
+     * @param parametros: Valores que acompañan a dicho comando
+     * @param numEsperado: Número de parámetros esperados
+     * @param validacionAdicional: Una función opcional para validación adicional(en este caso, la verificación de enteros)
+     */
     private fun verificarNumeroParametros(
         comando: String,
         parametros: Array<String>,
@@ -42,6 +63,12 @@ class ParseadorArgumentos : Salida {
         return arrayOf(comando, *parametros)
     }
 
+    /**
+     * Método privado que comprueba si un valor es un entero
+     *
+     * @param valor: Valor a verificar si es entero
+     * @param id: Id del comando a verificar
+     */
     private fun verificarEsEntero(
         valor: String,
         id: String,
